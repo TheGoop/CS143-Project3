d56961d2ad9f7d9de3a2e4ad67cdce1f15fd8368
Akshay Gupta <akshaygupta.us@gmail.com>
2021-11-05 11:58:22 -0700

    finished creating all of the tables, need to restart computer and test.

--- load.sql
+++ load.sql
@@ -17,5 +17,38 @@ create table Person(
     givenName varchar(255),
     familyName varchar(255)
     gender varchar(6) -- we only have male/female in our data
-)
+);
+
+create table Organization(
+    id int primary key, 
+    orgName varchar(255)
+);
+
+create table Birth(
+    id int primary key,
+    birthdate date, 
+    pid int
+);
+
+create table Place(
+    id int primary key, 
+    city varchar(255),
+    country varchar(255)
+);
+
+create table Prize(
+    id int primary key, 
+    year int, 
+    category varchar(255),
+    sortOrder int, 
+    aid int 
+);
+
+create table Affiliations(
+    id int primary key, 
+    affiliationName varchar(255),
+    pid int 
+);
+
+
 
