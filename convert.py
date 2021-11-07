331bd182e59731a1689b4d675d9837312a9fcd1f
Akshay Gupta <akshaygupta.us@gmail.com>
2021-11-07 01:37:24 -0800

    removed primary key to allow prize to have multiple affiliations

--- convert.py
+++ convert.py
@@ -138,16 +138,16 @@ def handleData(data):
             birthTuple = (attrs["id"], attrs["birthDate"], placeID)
             if debug: print (f"Birth: {birthTuple}")
             birthSet.add(birthTuple)
-
+            print("Person: " + str(attrs["id"]))
             for prize in attrs["nobelPrizes"]:
                 laureateTuple = (attrs["id"], pid)
                 if debug: print (f"Laureate: {laureateTuple}")
                 laureateSet.add(laureateTuple)
-
+                print ("Prize:")
                 for affiliate in prize["affiliations"]:
                     affiliateID = hash(affiliate["name"]) % 2147483647
                     prizeTuple = (pid, prize["awardYear"], prize["category"], prize["sortOrder"], affiliateID)
-                    if debug: print (f"Prize: {prizeTuple}")
+                    print (f"Prize: {prizeTuple}")
                     prizeSet.add(prizeTuple)
 
                     placeID = makePlaceID(affiliate["city"], affiliate["country"])
@@ -155,7 +155,7 @@ def handleData(data):
                     if debug: print(f"Affiliate Place: {placeTuple}")
                     placeSet.add(placeTuple)
                     affiliateTuple = (affiliateID, affiliate["name"], placeID)
-                    if debug: print(f"Affiliate: {affiliateTuple}")
+                    print(f"Affiliate: {affiliateTuple}")
                     affiliationSet.add(affiliateTuple)
 
                 pid += 1
